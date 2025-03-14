96a5d3e80d9dae94039fccd6b2e9c6931bc4b20d
PedroStonks <pedrohenriquemigue76@gmail.com>
2025-03-14 09:06:12 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,48 +1,106 @@
-import { Component } from 'react';
-import { StyleSheet, Text, View, ScrollView, Image, Button } from 'react-native';
-
-
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, FlatList, Image, Button } from 'react-native';
 
 class Noticias extends Component {
-
+  
   constructor(props) {
-    super(props)
-    this.state = { texto: 'Mega-acidente de trem na Índia mata 275 pessoas',imagem: 'https://midias.correiobraziliense.com.br/_midias/jpg/2023/12/27/cbifot250820230472-33746900.jpg' ,noticia:'A missão indiana Chandrayaan-3 fez história ao se tornar a primeira a conseguir pousar uma espaçonave nos arredores do polo sul da Lua.Até então, EUA, a antiga União Soviética e a China haviam conseguido chegar perto do equador lunar, mas nenhuma missão com destino ao polo sul foi bem-sucedida.'}
-    this.entrar = this.entrar.bind(this);
+    super(props);
+    this.state = {
+      noticiaAtual: {
+        titulo: 'Mega-acidente de trem na Índia mata 275 pessoas',
+        imagem: 'https://midias.correiobraziliense.com.br/_midias/jpg/2023/12/27/cbifot250820230472-33746900.jpg',
+        descricao:
+          'A missão indiana Chandrayaan-3 fez história ao se tornar a primeira a conseguir pousar uma espaçonave nos arredores do polo sul da Lua.',
+      },
+      noticias: [
+        {
+          titulo: 'Doença misteriosa mata mais de 50 pessoas no Congo',
+          imagem: 'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1zLlcI.img?w=1920&h=1080&q=60&m=2&f=webp',
+          descricao:
+            'A Organização Mundial da Saúde (OMS) disse que mais de 50 pessoas morreram no noroeste do Congo devido a uma doença desconhecida de rápida disseminação.',
+        },
+        {
+          titulo: 'Por que o rapper Oruam foi preso novamente?',
+          imagem: 'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1zPClv.img?w=768&h=511&m=6&f=webp',
+          descricao:
+            'Oruam, aos 23 anos, é considerado um dos principais nomes do rap e trap nacional. Seu nome de batismo é Mauro Davi dos Santos Nepomuceno.',
+        },
+        {
+          titulo: 'Paraibano analfabeto cria moto movida a água',
+          imagem: 'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1zMmMs.img?w=800&h=435&q=60&m=2&f=webp',
+          descricao:
+            'Sandro Alves de Oliveira, morador de Alagoa Nova, na Paraíba, criou um sistema que faz sua motocicleta funcionar com água.',
+        },
+      ],
+    };
   }
 
-  entrar(texto,imagem,noticia) {
-    this.setState({texto:texto, imagem:imagem,noticia:noticia})
+  atualizarNoticia = (noticia) => {
+    this.setState({ noticiaAtual: noticia });
+  };
 
-  }
+
   render() {
     return (
-      <ScrollView>
-        <View style ={{padding:50}}>
-          <Foto texto ={this.state.texto} imagem={this.state.imagem} noticia={this.state.noticia}/>
-          <Text style={{ marginTop: 50,}}></Text>
-          <Button title='Entrar' onPress={() => this.entrar('Doença misteriosa mata mais de 50 pessoas em poucas horas na República Democrática do Congo','https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1zLlcI.img?w=1920&h=1080&q=60&m=2&f=webp','A Organização Mundial da Saúde (OMS) disse que mais de 50 pessoas morreram no noroeste do Congo devido a uma doença desconhecida de rápida disseminação. Serge Ngalebato, diretor médico do Hospital Bikoro, um centro regional de monitoramento, afirmou que o curto período entre os sintomas e a morte é "muito preocupante". A OMS alerta que o surto pode ser causado por outro vírus que passou de animais para humanos. A OMS relata que o surto começou no país em 21 de janeiro, com 419 casos registrados até meados de fevereiro. Até agora, 53 pessoas morreram, dando à doença uma taxa de mortalidade de 12,49%, que é muito maior do que os 3,14% da COVID-19, em comparação.')}> </Button>
-          <Text style={{marginTop: 8}}></Text>
-          <Button title='Entrar' onPress={() => this.entrar('Por que o rapper Oruam foi preso novamente no Rio de Janeiro?','https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1zPClv.img?w=768&h=511&m=6&f=webp','Oruam, aos 23 anos, é considerado um dos principais nomes do rap e trap nacional. Seu nome de batismo é Mauro Davi dos Santos Nepomuceno e, ao longo de sua trajetória se envolveu em polêmcias, em especial, também por ser filho do traficante Marcinho VP, preso desde 1996.')}> </Button>
-          <Text style={{marginTop: 8}}></Text>
-          <Button title='Entrar' onPress={() => this.entrar('Paraibano analfabeto cria moto movida a água','https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1zMmMs.img?w=800&h=435&q=60&m=2&f=webp','Sandro Alves de Oliveira, morador de Alagoa Nova, no Agreste da Paraíba, é conhecido na cidade como “Sandro das antenas” ou “da moto movida a água”. Ele é o responsável pela criação de um sistema que faz a motocicleta dele funcionar com água, utilizando um reator de alumínio para transformar as moléculas de hidrogênio em combustível. A invenção veio após a greve dos caminhoneiros, em maio deste ano, quando a escassez de gasolina e o alto preço do combustível o motivaram a buscar alternativas.')}> </Button>
-        </View>
-      </ScrollView>
-    );
-  }
-}
-
-export default Noticias;
-
-class Foto extends Component {
-  render() {
-    return (
-      <View>
-        <Text style={{ marginTop: 50, fontSize: 25, color: '#000000', textAlign: 'center'}}>{this.props.texto}</Text>
-        <Image source={{ uri: this.props.imagem }}
-          style={{ width: 300, height: 300, marginTop: 20 ,borderRadius: 15}}></Image>
-        <Text style = {{fontSize:13, marginTop:20}}>{this.props.noticia}</Text>
+      <View style={estilos.container}>
+        <Noticia noticia={this.state.noticiaAtual} />
+        <FlatList
+          data={this.state.noticias}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <View style={estilos.botaoContainer}>
+              <Button title={item.titulo} onPress={() => this.atualizarNoticia(item)} />
+            </View>
+          )}
+        />
       </View>
     );
   }
 }
+
+const Noticia = ({ noticia }) => {
+  return (
+    <View style={estilos.noticiaContainer}>
+      <Text style={estilos.titulo}>{noticia.titulo}</Text>
+      <Image source={{ uri: noticia.imagem }} style={estilos.imagem} />
+      <Text style={estilos.descricao}>{noticia.descricao}</Text>
+    </View>
+  );
+};
+
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  noticiaContainer: {
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  titulo: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#333',
+    marginBottom: 10,
+  },
+  imagem: {
+    width: 300,
+    height: 200,
+    borderRadius: 10,
+  },
+  descricao: {
+    fontSize: 14,
+    textAlign: 'center',
+    color: '#555',
+    marginTop: 10,
+  },
+  botaoContainer: {
+    marginVertical: 5,
+    width: '90%',
+  },
+});
+
+export default Noticias;
